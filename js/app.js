4822c24abb280aab9e2b0f8017e7f61ab78e429f
Sachin <jadhavsachin174@gmail.com>
2019-09-24 10:09:37 +0530

    added stars file to the components + added comments to the moon and cloud files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function () {
     // $("#includedContent").load("components/cloud.html");
-    $("#includedContent").load("components/moon.html");
+    // $("#includedContent").load("components/moon.html");
+    $("#includedContent").load("components/stars.html");
 });
 
 
