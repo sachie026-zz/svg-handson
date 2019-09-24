ce251dc448f1bf64fb4ff2ebd215218ee05d7b55
Sachin <jadhavsachin174@gmail.com>
2019-09-24 21:23:30 +0530

    added character and animation for sad and happy mood

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 $(function () {
     // $("#includedContent").load("components/cloud.html");
     // $("#includedContent").load("components/moon.html");
-    $("#includedContent").load("components/stars.html");
+    // $("#includedContent").load("components/stars.html");
+    $("#includedContent").load("components/character-sad-happy.html");
 });
 
 
